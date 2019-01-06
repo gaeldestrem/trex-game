198176f8189ae2541c8e9328cf55c68d46dd3794
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-06 14:08:50 +0000

    Deploy gaeldestrem/trex-game to github.com/gaeldestrem/trex-game.git:gh-pages

---
+++ precache-manifest.7ec597437f918872d0302db08f07f7bf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e3ff1460d3c030421f5",
+    "url": "/trex-game/static/js/runtime~main.7e3ff146.js"
+  },
+  {
+    "revision": "e1e2cb0b0996cca4686d",
+    "url": "/trex-game/static/js/main.e1e2cb0b.chunk.js"
+  },
+  {
+    "revision": "b826ab092081ec944e03",
+    "url": "/trex-game/static/js/1.b826ab09.chunk.js"
+  },
+  {
+    "revision": "a850bc12cfdef37a1f2a9d455dc946d0",
+    "url": "/trex-game/index.html"
+  }
+];
